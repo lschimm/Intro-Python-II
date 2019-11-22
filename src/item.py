ac2fa1b63cee87ee014f43024e2db8d3a7cb8ed0
lschimm <lschimmelfennig@gmail.com>
2019-11-21 20:29:41 -0600

    Adds subclasses to item.py

--- src/item.py
+++ src/item.py
@@ -5,3 +5,15 @@ class Item:
 
     def __str__(self):
         return f"Item name: {self.name}, Item Description: {self.description}"
+
+class Sword(Item):
+    def __init__(self, name, description):
+        super().__init__(name, description, damage)
+        self.name = name
+        self.damage = damage
+
+class Shield(Item):
+    def __init__(self, name, description):
+        super().__init__(name, description, block)
+        self.name = name
+        self.block = block
